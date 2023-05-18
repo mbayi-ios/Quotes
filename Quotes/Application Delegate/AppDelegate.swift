e74e8e49ac9b3bf0c3b6febe843390ec7b99f468
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2023-05-18 17:46:47 +0300

    first commit

---
+++ Quotes/Application Delegate/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Quotes
+//
+//  Created by Bart Jacobs on 30/05/2019.
+//  Copyright © 2019 Code Foundry. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    // MARK: - Properties
+    
+    var window: UIWindow?
+
+    // MARK: - Application Life Cycle
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
